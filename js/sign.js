25b77340fc8ec7965890328d8a0cbb2c89136bfa
freedomxuli <xl1508107377@163.com>
2018-06-19 23:33:28 +0800

    优化

--- js/sign.js
+++ js/sign.js
@@ -2,10 +2,10 @@ function GetSign (params){
 	var private_key = "eqinfo1g2j3ew5rtyg06tofkvm479rifkv3b4v56cx8e7";
 	var param = assemble(params);
 /*	console.log(param);
-	console.log("数组加密："+hex_md5(param));
-	console.log("数组加密大写："+hex_md5(param).toUpperCase());
-	console.log("数组加密大写后加私钥："+hex_md5(param).toUpperCase()+private_key);*/
-	return hex_md5(hex_md5(param).toUpperCase()+private_key).toUpperCase();
+	console.log("数组加密："+md5(param));
+	console.log("数组加密大写："+md5(param).toUpperCase());
+	console.log("数组加密大写后加私钥："+md5(param).toUpperCase()+private_key);*/
+	return md5(md5(param).toUpperCase()+private_key).toUpperCase();
 }
 
 function assemble(params)
